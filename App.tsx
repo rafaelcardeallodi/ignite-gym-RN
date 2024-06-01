7bdbdc3096a20c96b4ffad242258c895df1a8cdd
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-06-01 02:20:03 -0300

    Imagem de Background

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { Loading } from '@components/loading';
 import { theme } from './src/theme';
+import { SignIn } from '@screens/sign-in';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/sign-in.tsx
@@ -0,0 +1,16 @@
+import { Image, VStack } from "native-base";
+
+import BackgroundImg from '@assets/background.png'
+
+export function SignIn (){
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image 
+        source={BackgroundImg}
+        alt="Pessoas treinando em uma academia"
+        resizeMode="contain"
+        position="absolute"
+      />
+    </VStack>
+  )
+}
