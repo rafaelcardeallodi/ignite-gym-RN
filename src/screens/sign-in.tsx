1d6642aa72474c35674e70093f045b472397fd47
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-06-01 03:21:56 -0300

    Definindo a imagem padrão

--- src/screens/sign-in.tsx
+++ src/screens/sign-in.tsx
@@ -22,6 +22,7 @@ export function SignIn() {
       <VStack flex={1} px={10} pb={16}>
         <Image 
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando em uma academia"
           resizeMode="contain"
           position="absolute"
