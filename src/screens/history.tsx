b3e68e6c2e302bcba0eedced41322166e87f2cf2
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-06-02 21:22:03 -0300

    Componente HistoryCard

--- src/screens/history.tsx
+++ src/screens/history.tsx
@@ -1,3 +1,4 @@
+import { HistoryCard } from "@components/history-card";
 import { ScreenHeader } from "@components/screen-header";
 import { VStack } from "native-base";
 
@@ -5,6 +6,11 @@ export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   )
 }
